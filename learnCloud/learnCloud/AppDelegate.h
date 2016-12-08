c9b73e6264109a6211d634efc52ad715faef9ecc
machao <machaomc89@sina.com>
2016-12-08 14:09:28 +0800

    添加工程文件

---
+++ learnCloud/learnCloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learnCloud
+//
+//  Created by macForJZZQ on 2016/12/8.
+//  Copyright © 2016年 macForJZZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learnCloud/learnCloud/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  learnCloud
+//
+//  Created by macForJZZQ on 2016/12/8.
+//  Copyright © 2016年 macForJZZQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
